a0115b439236937c151e2b3d69255c5d73c1e2ff
Krishna Chegonda <krishna.chegonda@pearson.com>
2023-03-02 14:47:29 +0530

    Add project files.

---
+++ OOPsConcepts/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPsConcepts
+{
+    class Program : A //, B - Abstract classes wont spport Mltpleinhertitance
+    {
+        static void Main(string[] args)
+        {
+
+            
+        }
+
+        public override void MyMethod()
+        {
+            Console.WriteLine();
+        }
+    }
+    abstract class A
+    {
+        public abstract void MyMethod();
+    }
+
+    abstract class B
+    {
+        public abstract void MyMethod();
+    }
+
+    
+}
